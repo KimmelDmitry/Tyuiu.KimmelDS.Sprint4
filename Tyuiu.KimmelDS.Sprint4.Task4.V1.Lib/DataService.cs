b07b757298822c71421437bce2aae147fd787ed0
EvilHui <Ukrumoski@gmail.com>
2024-11-29 18:23:43 +0500

    Task 4

---
+++ Tyuiu.KimmelDS.Sprint4.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KimmelDS.Sprint4.Task4.V1.Lib
+{
+    public class DataService : ISprint4Task4V1
+    {
+        public int Calculate(int[,] matrix) =>
+            Enumerable.Range(0, matrix.GetLength(0)) 
+                     .SelectMany(i => Enumerable.Range(0, matrix.GetLength(1)) 
+                     .Select(j => matrix[i, j])) 
+                     .Where(x => x % 2 == 0) 
+                     .Sum(); 
+
+
+    }
+}
