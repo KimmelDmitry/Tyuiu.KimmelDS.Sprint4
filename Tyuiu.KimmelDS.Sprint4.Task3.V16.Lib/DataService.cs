e8f0dd930e76ea365797c37a710fbb786f3040b9
EvilHui <Ukrumoski@gmail.com>
2024-11-29 18:08:46 +0500

    task 3

---
+++ Tyuiu.KimmelDS.Sprint4.Task3.V16.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KimmelDS.Sprint4.Task3.V16.Lib
+{
+    public class DataService : ISprint4Task3V16
+    {
+        public int Calculate(int[,] array) => 
+            Enumerable.Range(0, array.GetLength(0)) 
+                .Select(i => array[i, array.GetLength(1) - 1])  
+                .Min(); 
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint4.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KimmelDS.Sprint4.Task3.V16.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
