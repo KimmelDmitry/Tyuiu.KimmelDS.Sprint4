02da90522e27983b1115c2e11b7ab4d3b8019a2b
EvilHui <Ukrumoski@gmail.com>
2024-11-29 19:28:59 +0500

    task 7

---
+++ Tyuiu.KimmelDS.Sprint4.Task7.V27.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KimmelDS.Sprint4.Task7.V27.Lib
+{
+    public class DataService : ISprint4Task7V27
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    matrix[i, j] = (int)(value[i * m + j] - '0'); 
+
+            return Enumerable.Range(0, n).SelectMany(i =>
+                   Enumerable.Range(0, m).Select(j => matrix[i, j]))
+                   .Count(x => x % 2 == 0);
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint4.Task7.V27/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.KimmelDS.Sprint4.Task7.V27.Lib;
+namespace Tyuiu.KimmelDS.Sprint4.Task7.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Операции сравнения                                                      *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Киммель Дмитрий Сергеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу из операций сравнений (==, !=, <, >, <=, >=, последова*");
+            Console.WriteLine("*ательность операций не должна нарушаться) и арифметических выражений, кот*");
+            Console.WriteLine("*торая вернет логическую последовательность(массив): (True, True, True, Tr*");
+            Console.WriteLine("*rue, True, False), при x = 1054, y = 375                                 *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(4, 3, "583197256891");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
