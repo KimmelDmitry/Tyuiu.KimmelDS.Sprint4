b5ee864f2aaa589d8023bb81a9bf96cafe94b847
EvilHui <Ukrumoski@gmail.com>
2024-11-29 17:29:33 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KimmelDS.Sprint4.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,9 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KimmelDS.Sprint4.Task0.V10.Lib
+{
+    public class DataService : ISprint4Task0V10
+    {
+        public int GetSumOddArrEl(int[] array) => array.Where(x => x % 2 == 0).Sum();   
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint4.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KimmelDS.Sprint4.Task0.V10.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
